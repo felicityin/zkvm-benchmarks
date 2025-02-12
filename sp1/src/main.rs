8668f9562fe6817665a184684bd85cae2c69308c
Jingping Yin <yinjingping2022@gmail.com>
2025-02-12 13:48:42 +0800

    tmp

--- sp1/src/main.rs
+++ sp1/src/main.rs
@@ -14,39 +14,40 @@ const BIGMEM_ELF: &[u8] = include_elf!("bigmem");
 fn main() {
     init_logger();
 
-    // 1 Shard
-    let iters = [230, 460, 920, 1840, /* 3680 */ ];
-    let shard_sizes = [1 << 20, 1 << 21, 1 << 22, 1 << 23, /* 1 << 24 */]; // Max shard_size = 2^24-1
-    benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_1_shard.csv", "iters");
+    // // 1 Shard
+    // let iters = [230, 460, 920, 1840, /* 3680 */ ];
+    // let shard_sizes = [1 << 20, 1 << 21, 1 << 22, 1 << 23, /* 1 << 24 */]; // Max shard_size = 2^24-1
+    // benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_1_shard.csv", "iters");
 
-    // 2 Shards
-    let iters = [230, 460, 920, 1840, 3680];
-    let shard_sizes = [1 << 19, 1 << 20, 1 << 21, 1 << 22, 1 << 23];
-    benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_2_shard.csv", "iters");
+    // // 2 Shards
+    // let iters = [230, 460, 920, 1840, 3680];
+    // let shard_sizes = [1 << 19, 1 << 20, 1 << 21, 1 << 22, 1 << 23];
+    // benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_2_shard.csv", "iters");
 
-    // 4 Shards
-    let shard_sizes = [1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22];
-    benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_4_shard.csv", "iters");
+    // // 4 Shards
+    // let shard_sizes = [1 << 18, 1 << 19, 1 << 20, 1 << 21, 1 << 22];
+    // benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_4_shard.csv", "iters");
 
-    // 8 Shards
-    let shard_sizes = [1 << 17, 1 << 18, 1 << 19, 1 << 20, 1 << 21];
-    benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_8_shard.csv", "iters");
+    // // 8 Shards
+    // let shard_sizes = [1 << 17, 1 << 18, 1 << 19, 1 << 20, 1 << 21];
+    // benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_8_shard.csv", "iters");
 
-    // 16 Shards
-    let shard_sizes = [1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20];
-    benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_16_shard.csv", "iters");
+    // // 16 Shards
+    // let shard_sizes = [1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20];
+    // benchmark_with_shard_size(benchmark_sha2_chain, &iters, &shard_sizes, "../benchmark_outputs/sha2_chain_sp1_16_shard.csv", "iters");
 
-    benchmark(benchmark_sha3_chain, &iters, "../benchmark_outputs/sha3_chain_sp1.csv", "iters");
+    // benchmark(benchmark_sha3_chain, &iters, "../benchmark_outputs/sha3_chain_sp1.csv", "iters");
 
-    let lengths = [32, 256, 512, 1024, 2048];
-    benchmark(benchmark_sha2, &lengths, "../benchmark_outputs/sha2_sp1.csv", "byte length");
-    benchmark(benchmark_sha3, &lengths, "../benchmark_outputs/sha3_sp1.csv", "byte length");
+    // let lengths = [32, 256, 512, 1024, 2048];
+    // benchmark(benchmark_sha2, &lengths, "../benchmark_outputs/sha2_sp1.csv", "byte length");
+    // benchmark(benchmark_sha3, &lengths, "../benchmark_outputs/sha3_sp1.csv", "byte length");
 
-    let ns = [100, 1000, 10000, 50000];
+    // let ns = [100, 1000, 10000, 50000];
+    let ns = [10];
     benchmark(bench_fibonacci, &ns, "../benchmark_outputs/fibonacci_sp1.csv", "n");
 
-    let values = [5u32];
-    benchmark(bench_bigmem, &values, "../benchmark_outputs/bigmem_sp1.csv", "value");
+    // let values = [5u32];
+    // benchmark(bench_bigmem, &values, "../benchmark_outputs/bigmem_sp1.csv", "value");
 }
 
 fn init_logger() {
--- zkm/src/main.rs
+++ zkm/src/main.rs
@@ -48,19 +48,20 @@ fn main() {
     let _ = std::fs::remove_dir_all("/tmp/zkm.old");
     let _ = std::fs::rename("/tmp/zkm", "/tmp/zkm.old");
 
-    let lengths = [32, 256, 512, 1024, 2048];
-    benchmark(benchmark_sha2, &lengths, "../benchmark_outputs/sha2_zkm.csv", "byte length");
-    benchmark(benchmark_sha3, &lengths, "../benchmark_outputs/sha3_zkm.csv", "byte length");
+    // let lengths = [32, 256, 512, 1024, 2048];
+    // benchmark(benchmark_sha2, &lengths, "../benchmark_outputs/sha2_zkm.csv", "byte length");
+    // benchmark(benchmark_sha3, &lengths, "../benchmark_outputs/sha3_zkm.csv", "byte length");
 
-    let ns = [100, 1000, 10000, 50000];
+    // let ns = [100, 1000, 10000, 50000];
+    let ns = [10];
     benchmark(benchmark_fibonacci, &ns, "../benchmark_outputs/fiboancci_zkm.csv", "n");
 
-    let values = [5];
-    benchmark(benchmark_bigmem, &values, "../benchmark_outputs/bigmem_zkm.csv", "value");
+    // let values = [5];
+    // benchmark(benchmark_bigmem, &values, "../benchmark_outputs/bigmem_zkm.csv", "value");
 
-    let iters = [230, 460, 920, 1840, 3680];
-    benchmark(benchmark_sha2_chain, &iters, "../benchmark_outputs/sha2_chain_zkm.csv", "iters");
-    benchmark(benchmark_sha3_chain, &iters, "../benchmark_outputs/sha3_chain_zkm.csv", "iters");
+    // let iters = [230, 460, 920, 1840, 3680];
+    // benchmark(benchmark_sha2_chain, &iters, "../benchmark_outputs/sha2_chain_zkm.csv", "iters");
+    // benchmark(benchmark_sha3_chain, &iters, "../benchmark_outputs/sha3_chain_zkm.csv", "iters");
 }
 
 fn prove_single_seg_common(
