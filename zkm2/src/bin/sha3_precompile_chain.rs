1dcf498888e5b532b218705832cec4b8eeb1a42b
Jingping Yin <yinjingping2022@gmail.com>
2025-04-14 16:58:16 +0800

    fix benchmark_sha3_precompile_chain

--- zkm2/src/bin/sha3_precompile_chain.rs
+++ zkm2/src/bin/sha3_precompile_chain.rs
@@ -1,9 +1,9 @@
 use utils::benchmark_v2;
-use zkm_script::{benchmark_sha3_chain, init_logger};
+use zkm_script::{benchmark_sha3_precompile_chain, init_logger};
 
 fn main() {
     init_logger();
 
     let iters = [230, 460, /* 920, 1840,  3680 */];
-    benchmark_v2(benchmark_sha3_chain, &iters, "../benchmark_outputs/sha3_chain_zkm2.csv", "iters");
+    benchmark_v2(benchmark_sha3_precompile_chain, &iters, "../benchmark_outputs/sha3_precompile_chain_zkm2.csv", "iters");
 }
